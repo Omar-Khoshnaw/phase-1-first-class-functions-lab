641d422467bfeb143329eacd6021edc3ca7fbedf
Omar Sardar <omar.khoshnaw@gmail.com>
2024-03-11 22:16:30 +0300

    Assignment Completed

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = (arr) => {
+  return [arr[0], arr[1]];
+};
+
+const returnLastTwoDrivers = (arr) => {
+  return [arr[arr.length - 2], arr[arr.length - 1]];
+};
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+const createFareMultiplier = (x) => {
+  return (num) => {
+    return x * num;
+  };
+};
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+const selectDifferentDrivers = (drivers, fn) => {
+  return fn(drivers);
+};
